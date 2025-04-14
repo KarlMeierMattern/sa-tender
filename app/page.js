b96bf86e97f0be81afeedcd3efd3a5498e02a7d8
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-04-14 18:37:53 +0200

    commit

--- app/page.js
+++ app/page.js
@@ -2,16 +2,13 @@ import { Suspense } from "react";
 import TenderLayout from "./components/TenderLayout";
 import TableSkeleton from "./components/ui/table-skeleton";
 
-export default async function TendersPage({ searchParams }) {
-  const params = await searchParams;
-  const page = parseInt(params.page) || 1;
-
+export default function TendersPage() {
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold pl-2">SA Government Tenders Database</h1>
       <div className="mt-8">
         <Suspense fallback={<TableSkeleton />}>
-          <TenderLayout initialPage={page} />
+          <TenderLayout />
         </Suspense>
       </div>
     </div>
