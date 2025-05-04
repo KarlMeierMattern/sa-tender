fd254d9629dc8a1645846173a04157bade82a0f7
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-05-04 20:14:03 +0200

    commit

--- app/api/test/route.js
+++
@@ -1,7 +0,0 @@
-import { scrapeAwardedTendersCopy } from "@/app/lib/scrapers/tenders-awarded-copy";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const response = await scrapeAwardedTendersCopy();
-  return NextResponse.json({ data: response });
-}
