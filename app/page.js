a84ae2ffba79b74311ab960b5cae5344f3821db3
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-05-05 18:25:07 +0200

    commit

--- app/page.js
+++ app/page.js
@@ -1,15 +1,13 @@
 import { Suspense } from "react";
 import TenderLayout from "./components/TenderLayout";
-import TableSkeleton from "./components/ui/table-skeleton";
+// import TableSkeleton from "./components/ui/table-skeleton";
 
 export default function TendersPage() {
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold pl-2">SA Government Tenders Database</h1>
       <div className="mt-8">
-        <Suspense fallback={<TableSkeleton />}>
-          <TenderLayout />
-        </Suspense>
+        <TenderLayout />
       </div>
     </div>
   );
