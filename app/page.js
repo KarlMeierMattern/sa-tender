7d08e9fc8869dd1f56f57be66fbfae1a4ca522f3
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-08-23 16:00:44 +0200

    commit

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import TenderLayout from "./components/TenderLayout";
 export default function TendersPage() {
   return (
     <div className="p-4">
-      <h1 className="text-xl font-bold pl-2">SA Government Tenders Database</h1>
+      <h1 className="text-xl font-bold pl-2">SA Government Tenders</h1>
       <div className="mt-8">
         <Suspense>
           <TenderLayout />
